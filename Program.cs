c63f81d7c970fa2ccecf677c9fa08ad20bcfee8f
Yulia Batrakova <julichka_71@mail.ru>
2022-02-21 15:19:15 +0300

    String metho

--- Program.cs
+++ Program.cs
@@ -25,7 +25,29 @@ int FirstStringArray(string[] stringArray)
     }
     return numberOfStrings;
 }
+string[] SecondStringArray (string[] stringArray, int sizeNewArray)
+{
+    string[] newStringArray = new string[sizeNewArray];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length < 4)
+        {
+            newStringArray[j] = stringArray[i];
+            j++;
+        } 
+    }
+    return newStringArray;
+}
 
+string[] stringArray = {"Hello", "z", "World",";-)", "green", "to"};
+PrintStringArray(stringArray);
+//WriteLine(SearchStringsWithSizeLessOrEqual3(stringArray));
+int sizeNewArray = FirstStringArray(stringArray);
+string[] newStringArray = SecondStringArray(stringArray,sizeNewArray);
+WriteLine(" -> ");
+PrintStringArray(newStringArray);
+WriteLine();
 
 
 
