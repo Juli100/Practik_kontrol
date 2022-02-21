674b9a4ccbcbc71c9f0795754e045262a0c9fbca
Yulia Batrakova <julichka_71@mail.ru>
2022-02-21 15:17:19 +0300

    1 Method added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+void PrintStringArray(string[] stringArray)
+{
+    Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Write($"\"{stringArray[i]}\"");
+        if (i!=stringArray.Length - 1)
+        {
+            Write(", ");
+        }
+    }
+    Write("]");
+}
+
+
+
+
